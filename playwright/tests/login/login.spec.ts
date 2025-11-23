4cb49be45b399582b334863b143c0a429cf3a05c
shubham gaikwad <shubhamggaikwad2002@gmail.com>
2025-11-24 01:01:32 +0530

    refactored login.spec.ts

--- playwright/tests/login/login.spec.ts
+++ playwright/tests/login/login.spec.ts
@@ -1,29 +1,42 @@
 import { test, expect } from './baseTest.ts';
-import Constants from '../../utils/constants/constatns';
+import Constants from '../../utils/constants/constatns.ts';
 
-test.describe('Login Tests', () => {
-});
+const { 
+    validUserNames, 
+    PASSWORD,
+    LOCKED_OUT_USER,
+    WRONG_PASSWORD,
+    WRONG_USERNAME,
+    INVENTORY_URL,
+    loginErrorMessages
+} = Constants; 
 
-Constants.validUserNames.forEach((username) => {
-    test(`Login Success Test for ${username}`, async ({loginPage}) => {
-        await loginPage.login(username, Constants.PASSWORD);
-        await expect(loginPage.page).toHaveURL(Constants.INVENTORY_URL);
+
+test.describe('Login Success Tests', () => {
+    Constants.validUserNames.forEach((username) => {
+        test(`Login Success Test for ${username}`, async ({loginPage}) => {
+            await loginPage.login(username, PASSWORD);
+            await expect(loginPage.page).toHaveURL(INVENTORY_URL);
+        });
     });
 });
 
-test('Login Locked Out User Test', async ({loginPage}) => {
-    await loginPage.login(Constants.LOCKED_OUT_USER, Constants.PASSWORD);
-    await expect(loginPage.errorMessage).toHaveText(Constants.loginErrorMessages.LOCKED_OUT);
-});
 
-Constants.validUserNames.forEach((username) => {
-    test(`Login Wrong Password Test for ${username}`, async ({loginPage}) => {
-        await loginPage.login(username, Constants.WRONG_PASSWORD);
-        await expect(loginPage.errorMessage).toHaveText(Constants.loginErrorMessages.INVALID_CREDENTIALS);
+test.describe('Login Failure Tests', () => {
+    test('Login Locked Out User Test', async ({loginPage}) => {
+        await loginPage.login(LOCKED_OUT_USER, PASSWORD);
+        await expect(loginPage.errorMessage).toHaveText(loginErrorMessages.LOCKED_OUT);
     });
-});
 
-test('Login wrong Username Test', async ({loginPage}) => {
-    await loginPage.login(Constants.WRONG_USERNAME, Constants.PASSWORD);
-    await expect(loginPage.errorMessage).toHaveText(Constants.loginErrorMessages.INVALID_CREDENTIALS);
+    validUserNames.forEach((username) => {
+        test(`Login Wrong Password Test for ${username}`, async ({loginPage}) => {
+            await loginPage.login(username, WRONG_PASSWORD);
+            await expect(loginPage.errorMessage).toHaveText(loginErrorMessages.INVALID_CREDENTIALS);
+        });
+    });
+
+    test('Login wrong Username Test', async ({loginPage}) => {
+        await loginPage.login(WRONG_USERNAME, PASSWORD);
+        await expect(loginPage.errorMessage).toHaveText(loginErrorMessages.INVALID_CREDENTIALS);
+    });
 });
